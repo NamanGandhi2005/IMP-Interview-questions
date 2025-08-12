b4336e5b82195cafe81af01bad7cb0a526ccceed
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-08-12 23:01:53 +0530

    Time: 400 ms (45.28%), Space: 185.8 MB (34.19%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -3,55 +3,44 @@ public:
     void merge(vector<int>&nums,int start,int end){
         int mid=start+(end-start)/2;
 
-        int left=mid-start+1;
-        int right=end-mid;
+        int leftSize=mid-start+1;
+        int rightSize=end-mid;
 
-        vector<int>leftArray(left);
-        vector<int>rightArray(right);
-
-        //copy values into both arrays
+        vector<int>left(leftSize);
+        vector<int>right(rightSize);
 
         int k=start;
 
-        for(int i=0;i<left;i++){
-            leftArray[i]=nums[k++];
+        for(int i=0;i<leftSize;i++){
+            left[i]=nums[k++];
         }
         k=mid+1;
-        for(int i=0;i<right;i++){
-            rightArray[i]=nums[k++];
+
+        for(int i=0;i<rightSize;i++){
+            right[i]=nums[k++];
         }
-
-        //now merge into main array by comparing
-
+        int mainArrayIndex=start;
         int index1=0;
         int index2=0;
-        int mainArrayIndex=start;
 
-        while(index1<left && index2<right){
-            if(leftArray[index1]<rightArray[index2]){
-                nums[mainArrayIndex++]=leftArray[index1++];
-
+        while(index1<leftSize && index2<rightSize){
+            if(left[index1]<right[index2]){
+                nums[mainArrayIndex++]=left[index1++];
             }
-            else if(rightArray[index2]<leftArray[index1]){
-                nums[mainArrayIndex++]=rightArray[index2++];
-            }
-            else {
-                nums[mainArrayIndex++]=leftArray[index1++];
-                // index2++;
+            else{
+                nums[mainArrayIndex++]=right[index2++];
             }
         }
-
-        while(index1<left){
-            nums[mainArrayIndex++]=leftArray[index1++];
+        
+        while(index1<leftSize){
+            nums[mainArrayIndex++]=left[index1++];
         }
-        while(index2<right){
-            nums[mainArrayIndex++]=rightArray[index2++];
+        while(index2<rightSize){
+            nums[mainArrayIndex++]=right[index2++];
         }
-        return;
-
 
     }
-
+    
     void mergeSort(vector<int>&nums,int start,int end){
         if(start>=end){
             return;
@@ -66,11 +55,8 @@ public:
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        //merge sort
         int n=nums.size();
-        int start=0;
-        int end=n-1;
-        mergeSort(nums,start,end);
+        mergeSort(nums,0,n-1);
         return nums;
     }
 };
