1aea85333866b10027f15f6b48b8f97d63f84dfb
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-08-12 13:06:29 +0530

    Time: 203 ms (40.87%), Space: 12.7 MB (42.07%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int countPrimes(int n) {
         vector<bool>prime(n+1,true);
-
-        prime[0]=false;
-        prime[1]=false;
+        prime[0]=prime[1]=false;
         int count=0;
         for(int i=2;i<n;i++){
             if(prime[i]){
@@ -12,9 +10,8 @@ public:
                 for(int j=2*i;j<n;j=j+i){
                     prime[j]=0;
                 }
-            }            
+            }
         }
         return count;
-
     }
 };
