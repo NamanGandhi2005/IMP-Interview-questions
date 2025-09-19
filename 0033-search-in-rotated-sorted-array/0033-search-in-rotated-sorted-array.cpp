bd1f06b7a505589a10e8169d3caf7e88b982f60e
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-09-20 00:19:16 +0530

    Time: 0 ms (100%), Space: 15.2 MB (32.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int pivotSolve(vector<int>&nums){
+    int findMin(vector<int>&nums){
         int n=nums.size();
 
         int start=0;
         int end=n-1;
 
         int mid=start+(end-start)/2;
+        
         while(start<end){
             if(nums[mid]>nums[end]){
                 start=mid+1;
             }
             else{
                 end=mid;
-
             }
             mid=start+(end-start)/2;
 
+
         }
+
         return mid;
     }
-
-    int binarySearch(vector<int>nums,int start,int end,int target){
+    int binarySearch(vector<int>&nums,int start,int end,int target){
         int mid=start+(end-start)/2;
-
-        // int index=-1;
+        int ans=-1;
         while(start<=end){
             if(nums[mid]==target){
                 return mid;
@@ -32,32 +32,27 @@ public:
             else if(nums[mid]<target){
                 start=mid+1;
             }
-            else if(nums[mid]>target){
+            else{
                 end=mid-1;
             }
             mid=start+(end-start)/2;
-
         }
         return -1;
     }
-
     int search(vector<int>& nums, int target) {
         int n=nums.size();
 
+        int index=findMin(nums);
 
-        int pivotIndex=pivotSolve(nums);
+        int left=binarySearch(nums,0,index-1,target);
+        int right=binarySearch(nums,index,n-1,target);
 
-        int leftIndex=binarySearch(nums,0,pivotIndex-1,target);
-        int rightIndex=binarySearch(nums,pivotIndex,n-1,target);
-        if(leftIndex==-1){
-            return rightIndex;
+        if(left==-1 && right==-1){
+            return -1;
         }
-        if(rightIndex==-1){
-            return leftIndex;
+        else if(left==-1){
+            return right;
         }
-        return -1;
-
-
-
+        return left;
     }
 };
