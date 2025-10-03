ed3cf431b6f14f5368b666befe83cd67fa95e5cd
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-04 02:29:44 +0530

    Time: 0 ms (100%), Space: 15.8 MB (33.05%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,23 +5,24 @@ public:
 
         int gola_index=-1;
 
-        for(int i=n-1;i>=1;i--){
-            if(nums[i]>nums[i-1]){
-                gola_index=i-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                gola_index=i;
                 break;
             }
         }
 
         if(gola_index!=-1){
             for(int i=n-1;i>gola_index;i--){
-                if(nums[gola_index]<nums[i]){
-                    swap(nums[i],nums[gola_index]);
+                if(nums[i]>nums[gola_index]){
+                    swap(nums[gola_index],nums[i]);
                     break;
                 }
             }
         }
 
-        reverse(nums.begin()+gola_index+1,nums.end());
 
+        reverse(nums.begin()+gola_index+1,nums.end());
+        
     }
 };
