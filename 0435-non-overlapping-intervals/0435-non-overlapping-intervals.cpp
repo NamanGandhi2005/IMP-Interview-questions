253f4c1ca3e608e0ed66c76ac82587085bc1df22
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-29 21:09:58 +0530

    Time: 169 ms (13.61%), Space: 119.3 MB (12.2%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,25 +3,28 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int n=intervals.size();
 
+
         int count=0;
 
         int i=0;
         int j=1;
         sort(intervals.begin(),intervals.end());
-
         while(j<n){
             vector<int>curr=intervals[i];
             vector<int>next=intervals[j];
+
             int cs=curr[0];
             int ce=curr[1];
             int ns=next[0];
             int ne=next[1];
 
             //non overlapping case
+
             if(ce<=ns){
                 i=j;
                 j++;
             }
+            //overlapping
             else if(ce<=ne){
                 count++;
                 j++;
@@ -31,7 +34,8 @@ public:
                 j++;
                 count++;
             }
-        }   
+        }
+
         return count;
     }
 };
