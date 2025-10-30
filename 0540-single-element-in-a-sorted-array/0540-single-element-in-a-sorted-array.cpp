543f268790fd782bac64b431452d5f3d8faed963
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-30 22:11:10 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n=nums.size();
+        //map methods create map and pick char 
 
+        //xor method ans=0; 
+        //xor with every element
+
+        //binaery search
+
+        int n=nums.size();
         int start=0;
         int end=n-1;
 
@@ -13,34 +19,33 @@ public:
             if((end-mid)%2==0){
                 isEven=true;
             }
-            else{
-                isEven=false;
-            }
 
             if(nums[mid]==nums[mid+1]){
-                //aabbc
                 if(isEven){
+                    //aabbc
                     start=mid+2;
                 }
                 else{
+                    //aabbcc
                     end=mid-1;
                 }
+
             }
             else{
+                
                 if(isEven){
                     //abbcc
                     end=mid;
-
                 }
                 else{
                     //abbccd
                     start=mid+1;
                 }
-
             }
             mid=start+(end-start)/2;
         }
 
         return nums[end];
+
     }
 };
