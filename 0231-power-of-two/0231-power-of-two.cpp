889aa252c96014fbae427aa27454c6b9252fa6a0
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-30 22:11:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (16.48%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,14 +3,15 @@ public:
     bool isPowerOfTwo(int n) {
         int ans=1;
 
-        for(int i=0;i<=30;i++){
+        for(int i=0;i<32;i++){
             if(ans==n){
                 return true;
             }
+
             if(ans<INT_MAX/2)
             ans=ans*2;
-
         }
+
         return false;
     }
 };
