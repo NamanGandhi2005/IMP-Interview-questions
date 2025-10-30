b69c378a47a76009d615978cfbf60edc6370092c
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-30 22:29:19 +0530

    Time: 0 ms (100%), Space: 63.5 MB (78.07%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -5,25 +5,20 @@ public:
 
         int start=0;
         int end=n-1;
-
         int mid=start+(end-start)/2;
         int ans=-1;
-
         while(start<end){
             if(arr[mid]<arr[mid+1]){
                 start=mid+1;
-
             }
-            else if(arr[mid]>arr[mid+1]){
-                //right side ki baat hori h aur ans bhi ho skta h
+            else{
                 ans=mid;
                 end=mid;
-
             }
             mid=start+(end-start)/2;
-
         }
+
         return ans;
-    
+
     }
 };
