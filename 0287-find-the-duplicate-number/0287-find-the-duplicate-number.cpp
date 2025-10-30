37dc7d69b75f08dafd9d9108436ae56bc76a7e8b
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-30 22:11:07 +0530

    Time: 5 ms (55.77%), Space: 65 MB (69.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
+        //map metho crate map that valu that count is 2 return
+        //sort the array and caompare with adjacent if equal then return 
+
+        //tortoise and hare algorithm
 
         int slow=nums[0];
         int fast=nums[0];
-        slow=nums[slow];
 
+
+        slow=nums[slow];
         fast=nums[nums[fast]];
+
 
         while(slow!=fast){
             slow=nums[slow];
@@ -15,6 +20,7 @@ public:
         }
 
         slow=nums[0];
+
         while(slow!=fast){
             slow=nums[slow];
             fast=nums[fast];
